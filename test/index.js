6e6b4d6073e4b10b136abc5a6b3385d71704c43c
Dmitry <df.creative@gmail.com>
2014-09-08 02:57:06 +0400

    Fix tests

--- test/index.js
+++ test/index.js
@@ -3,20 +3,20 @@ var assert = require('chai').assert;
 
 describe('Parsing', function(){
 	it('value', function(){
-		parse.value('123')
-		parse.value('true')
+		parse.value('123');
+		parse.value('true');
 	})
 	it('attribute', function(){
 		parse.attribute({})
 	})
 	it('object', function(){
-		parse.object('{1:1}')
+		parse.object('{1:1}');
 	})
 	it('list', function(){
-		parse.list('1,2,4')
+		parse.list('1,2,4');
 	})
 	it('stringify', function(){
-		parse.stringify({a:1})
-		parse.stringify([1,2,43])
+		parse.stringify({a:1});
+		parse.stringify([1,2,43]);
 	})
 })
