1468339fb6260769bc498bc5332115b65276f3e0
Dmitry <df.creative@gmail.com>
2014-09-08 02:51:19 +0400

    Strinp dangerous Function evals

--- index.js
+++ index.js
@@ -12,129 +12,138 @@ var isObject = type.isObject;
 var isBool = type.isBool;
 var dashed = str.dashed;
 
-var parse = module.exports = {
-	//parse attribute from the target
-	attribute: function(target, name, example){
-		var result;
+module.exports = {
+	value: parseValue,
+	attribute: parseAttr,
+	typed: parseTyped,
+	object: parseObject,
+	list: parseList,
+	stringify: stringify
+};
 
-		//parse attr value
-		if (!has(target, name)) {
-			if (has(target, 'attributes')) {
-				var dashedPropName = str.dashed(name);
+//parse attribute from the target
+function parseAttr(target, name, example){
+	var result;
 
-				var attr = target.attributes[name] || target.attributes['data-' + name] || target.attributes[dashedPropName] || target.attributes['data-' + dashedPropName];
+	//parse attr value
+	if (!has(target, name)) {
+		if (has(target, 'attributes')) {
+			var dashedPropName = str.dashed(name);
 
-				if (attr) {
-					// console.log('parseAttr', name, propType)
-					//fn on-attribute
-					if (/^on/.test(name)) {
-						target[name] = new Function(attr.value);
-					}
+			var attrs = target.attributes,
+				attr = attrs[name] || attrs['data-' + name] || attrs[dashedPropName] || attrs['data-' + dashedPropName],
+				attrVal = attr.value;
 
-					//detect based on type
-					else {
-						target[name] = parse.typed(attr.value, example);
-					}
-				}
+			if (attr) {
+				// console.log('parseAttr', name, propType)
+				//fn on-attribute
+				// if (/^on/.test(name)) {
+				// 	target[name] = new Function(attrVal);
+				// }
+
+				//detect based on type
+				// else {
+					target[name] = parseTyped(attrVal, example);
+				// }
 			}
 		}
+	}
 
-		return result;
-	},
+	return result;
+}
 
-	//returns value from string with correct type except for array
-	//TODO: write tests for this fn
-	value: function(str){
-		var v;
-		// console.log('parse', str)
-		if (/true/i.test(str)) {
-			return true;
-		} else if (/false/i.test(str)) {
-			return false;
-		} else if (!/[^\d\.\-]/.test(str) && !isNaN(v = parseFloat(str))) {
-			return v;
-		} else if (/\{/.test(str)){
-			try {
-				return JSON.parse(str)
-			} catch (e) {
-				return str
-			}
-		}
-		return str;
-	},
-
-	//parse value according to the type passed
-	typed: function(value, type){
-		var res;
-		// console.log('parse typed', value, type)
-		if (isArray(type)) {
-			res = parse.list(value);
-		} else if (isNumber(type)) {
-			res = parseFloat(value)
-		} else if (isBool(type)){
-			res = !/^(false|off|0)$/.test(value);
-		} else if (isFn(type)){
-			res = new Function(value);
-		} else if (isString(type)){
-			res = value;
-		} else if (isObject(type)) {
-			res = parse.object(value)
-		} else {
-			if (isString(value) && !value.length) res = true;
-			else res = parse.value(value);
-		}
-
-		return res;
-	},
-
-	object: function(str){
-		if (str[0] !== '{') str = '{' + str + '}';
+//returns value from string with correct type except for array
+//TODO: write tests for this fn
+function parseValue(str){
+	var v;
+	// console.log('parse', str)
+	if (/true/i.test(str)) {
+		return true;
+	} else if (/false/i.test(str)) {
+		return false;
+	} else if (!/[^\d\.\-]/.test(str) && !isNaN(v = parseFloat(str))) {
+		return v;
+	} else if (/\{/.test(str)){
 		try {
 			return JSON.parse(str);
 		} catch (e) {
-			return {}
-		}
-	},
-
-	//returns array parsed from string
-	list: function(str){
-		if (!isString(str)) return [parse.value(str)]
-
-		//clean str from spaces/array rudiments
-		str = str.trim();
-		if (str[0] === '[') str = str.slice(1);
-		if (str.length > 1 && str[str.length - 1] === ']') str = str.slice(0,-1);
-
-		var result = [];
-		eachCSV(str, function(value) {
-			result.push(parse.value(value))
-		})
-
-		return result;
-	},
-
-	//stringify any element passed, useful for attribute setting
-	stringify: function(el){
-		if (!el) {
-			return '' + el
-		} if (isArray(el)){
-			//return comma-separated array
-			return el.join(',')
-		} else if (isElement(el)){
-			//return id/name/proper selector
-			return el.id
-
-			//that way is too heavy
-			// return selector(el)
-		} else if (isObject(el)){
-			//serialize json
-			return JSON.stringify(el);
-		} else if (isFn(el)){
-			//return fn body
-			var src = el.toString();
-			el.slice(src.indexOf('{') + 1, src.lastIndexOf('}'));
-		} else {
-			return el.toString();
+			return str;
 		}
 	}
-};
+	return str;
+}
+
+//parse value according to the type passed
+function parseTyped(value, type){
+	var res;
+	// console.log('parse typed', value, type)
+	if (isArray(type)) {
+		res = parseList(value);
+	} else if (isNumber(type)) {
+		res = parseFloat(value);
+	} else if (isBool(type)){
+		res = !/^(false|off|0)$/.test(value);
+	} else if (isFn(type)){
+		res = value; //new Function(value);
+	} else if (isString(type)){
+		res = value;
+	} else if (isObject(type)) {
+		res = parseObject(value);
+	} else {
+		if (isString(value) && !value.length) res = true;
+		else res = parseValue(value);
+	}
+
+	return res;
+}
+
+function parseObject(str){
+	if (str[0] !== '{') str = '{' + str + '}';
+	try {
+		return JSON.parse(str);
+	} catch (e) {
+		return {};
+	}
+}
+
+//returns array parsed from string
+function parseList(str){
+	if (!isString(str)) return [parseValue(str)];
+
+	//clean str from spaces/array rudiments
+	str = str.trim();
+	if (str[0] === '[') str = str.slice(1);
+	if (str.length > 1 && str[str.length - 1] === ']') str = str.slice(0,-1);
+
+	var result = [];
+	eachCSV(str, function(value) {
+		result.push(parseValue(value));
+	});
+
+	return result;
+}
+
+//stringify any element passed, useful for attribute setting
+function stringify(el){
+	if (!el) {
+		return '' + el;
+	} if (isArray(el)){
+		//return comma-separated array
+		return el.join(',');
+	} else if (isElement(el)){
+		//return id/name/proper selector
+		return el.id;
+
+		//that way is too heavy
+		// return selector(el)
+	} else if (isObject(el)){
+		//serialize json
+		return JSON.stringify(el);
+	} else if (isFn(el)){
+		//return fn body
+		var src = el.toString();
+		el.slice(src.indexOf('{') + 1, src.lastIndexOf('}'));
+	} else {
+		return el.toString();
+	}
+}
